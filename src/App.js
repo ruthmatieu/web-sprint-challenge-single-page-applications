6da4a5ecf77fe6201eb5882a07f259ea9ac16f8d
matieuruth <matieu.ruth@gmail.com>
2020-09-13 19:23:15 -0400

    created links

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
+import { Route, Switch } from 'react-router-dom';
+
+//components
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import Form from './components/Form';
+import Confirmation from './components/Confirmation';
 
 const App = () => {
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <div className="App">
+      <Navbar/>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route path="/pizza"><Form/></Route>
+        <Route path="/confirmation"><Confirmation/></Route>
+      </Switch>
+    </div>
   );
 };
 export default App;
