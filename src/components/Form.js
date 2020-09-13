61c36e787143ef217112b44bcb886ba675b2da0b
matieuruth <matieu.ruth@gmail.com>
2020-09-13 19:37:44 -0400

    name input field created

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import axios from 'axios';
+import * as yup from "yup";
+import { Link } from 'react-router-dom';
 
 const Form = () => {
     return (
         <div>
 
+            <div>
+                <h2>Build your own pizza</h2>
+            </div>
+
+            <form>
+                {/* Name */}
+                <div>
+                    <h5 className="form-title">1. Name</h5>
+                    <label htmlFor="name">
+                        <input
+                            id="name"
+                            type="text"
+                            name="name"
+                            value=""
+                            onChange=""
+                        />
+                    </label><br/>
+                </div>
+
+                <div>
+                    
+                </div>
+            </form>
         </div>
     )
 }
