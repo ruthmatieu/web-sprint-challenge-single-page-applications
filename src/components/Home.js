f81e77f820b873f15874b42d742625daccc4321a
matieuruth <matieu.ruth@gmail.com>
2020-09-13 19:33:13 -0400

    completed home comp

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const Home = () => {
-    return (
-        <div>
 
-        </div>
+    const linkStyle = {
+        textDecoration: 'none',
+    }
+
+    return (
+        <section>
+            <div>
+                <h1>Your favorite food delivered <br/> while coding.</h1>
+                <Link to="/pizza" style={linkStyle}><button>Pizza</button></Link>
+            </div>
+        </section>
     )
 }
 
