4f9fe2c95deb45f7a6289de367a20d95b4dd5600
matieuruth <matieu.ruth@gmail.com>
2020-09-13 19:30:25 -0400

    completed navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 
 const Navbar = () => {
-    return (
-        <div>
 
-        </div>
-    )
-}
+    const linkStyle = {
+        textDecoration: 'none',
+        color: 'black'
+    }
+
+    return (
+        <nav>
+            <div className="left-nav">
+                <Link to="/" style={linkStyle}><li>Lambda Eats</li></Link>
+            </div>
+
+            <div className="right-nav">
+                <Link to="/" style={linkStyle}><li>Home</li></Link>
+                <Link to="/pizza" style={linkStyle}><li>Order</li></Link>
+            </div>
+        </nav>
+    );
+};
 
 export default Navbar;
