5ff1b75d34b44ecffe1ee63c6f0ad10208004b6a
matieuruth <matieu.ruth@gmail.com>
2020-09-13 19:38:58 -0400

    pizza size input added

--- src/components/Form.js
+++ src/components/Form.js
@@ -26,9 +26,25 @@ const Form = () => {
                     </label><br/>
                 </div>
 
+                {/* Pizza size */}
                 <div>
-                    
+                    <h4 className="form-title">2. Choice of size</h4>
+                    <label htmlFor="size">
+                        <select
+                            id="size"
+                            name="size"
+                            value={formInfo.size}
+                            onChange={changeHandler}
+                        >
+                            <option value={null}></option>
+                            <option value="16in">16 in</option>
+                            <option value="12in">12 in</option>
+                            <option value="10in">10 in</option>
+                            <option value="8in">8 in</option>
+                        </select>
+                    </label><br/>
                 </div>
+
             </form>
         </div>
     )
